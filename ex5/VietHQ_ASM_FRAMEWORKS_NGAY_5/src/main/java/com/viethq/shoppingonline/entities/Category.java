45a51e96f260c122bf21a650b6e7406693025790
viet <viethoangquoc2711@gmail.com>
2018-10-04 20:37:38 +0700

    - Add "ex 5"

---
+++ ex5/VietHQ_ASM_FRAMEWORKS_NGAY_5/src/main/java/com/viethq/shoppingonline/entities/Category.java
@@ -0,0 +1,34 @@
+package com.viethq.shoppingonline.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tblCategory")
+public class Category {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Category() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
